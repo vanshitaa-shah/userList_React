4c60ad757f92532cc67f2e4f15159de5b8b2fea9
vanshita shah <vanshita.s@simformsolutions.com>
2023-05-09 13:18:50 +0530

    Converted API fetch logic into action creator using createAsyncThunk

--- src/store/userSlice.js
+++ src/store/userSlice.js
@@ -1,19 +1,37 @@
-import {createSlice } from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 const userSlice = createSlice({
     name: 'user',
-    initialState: { users: [],pageCount:0,isLoading:true },
+    initialState: { users: [],pageCount:0,status:'',showPaginate:false },
     reducers: {
-        addUsers(state,action) {
-            state.users=action.payload;
-        },
         pageChangeHandler(state,action){
             state.pageCount=action.payload;
         },
-        toggleLoading(state){
-            state.isLoading=state.isLoading?!state.isLoading:state.isLoading;
-        }
+    },
+    extraReducers:(builder)=>{
+        builder.addCase(fetchUsers.pending,(state)=>{
+            state.status='loading';
+            state.showPaginate=false
+        })
+        .addCase(fetchUsers.fulfilled,(state,action)=>{
+            state.status='success';
+            state.showPaginate=true;
+            state.users=action.payload;
+        })
+        .addCase(fetchUsers.rejected,(state,action)=>{
+            state.status='failed';
+            state.showPaginate=false
+            state.users=[];
+        })
+
     }
+})
+
+
+export const fetchUsers=createAsyncThunk('user/fetchUsers',async(pageCount)=>{
+    const res= await fetch(`https://servers-omega.vercel.app/users/p?limit=7&page=${pageCount}`);
+    const data=await res.json();
+    return data.users;
 })
 
 export default userSlice.reducer;
